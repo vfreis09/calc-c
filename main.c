28b313adfd9edb58cb1355e34122bb511ae26137
vfreis09 <reis.vicente27@gmail.com>
2024-11-18 18:18:32 -0300

    switch cases added

--- main.c
+++ main.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
 
 int main(void){
+    char operator;
+    double operand1, operand2;
+
+    printf("Enter operator (+, -, *, /): ");
+
+    scanf("%c", &operator);
+
+    printf("Enter operands (numbers): ");
+
+    scanf("%lf %lf", &operand1, &operand2);
+
+    switch (operator){
+    case '+':
+        printf("%f + %f = %f\n",
+            operand1,
+            operand2,
+            operand1 + operand2);
+        break;
+
+    case '-':
+        printf("%f - %f = %f\n",
+            operand1,
+            operand2,
+            operand1 - operand2);
+        break;
+
+    case '*':
+        printf("%f * %f = %f\n",
+            operand1,
+            operand2,
+            operand1 * operand2);
+        break;
+
+    case '/':
+        printf("%f / %f = %f\n",
+            operand1,
+            operand2,
+            operand1 / operand2);
+        break;
+    
+    default:
+        printf("Invalid operator entered.\n");
+    }
+
     return 0;
 }
