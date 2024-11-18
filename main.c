c4bdc6eb521f5bc06a35179d0c1b3e31910c95a5
vfreis09 <reis.vicente27@gmail.com>
2024-11-18 18:35:17 -0300

    allow continuous calculations

--- main.c
+++ main.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
 
 int main(void){
-    char operator;
+    char operator, choice;
     double operand1, operand2;
 
-    printf("Enter operator (+, -, *, /): ");
+    do {
+        printf("Enter operator (+, -, *, /): ");
 
-    scanf("%c", &operator);
+        scanf(" %c", &operator);
 
-    printf("Enter operands (numbers): ");
+        printf("Enter operands (numbers): ");
 
-    scanf("%lf %lf", &operand1, &operand2);
+        scanf("%lf %lf", &operand1, &operand2);
 
-    switch (operator){
-    case '+':
-        printf("%f + %f = %f\n",
-            operand1,
-            operand2,
-            operand1 + operand2);
-        break;
+        switch (operator){
+        case '+':
+            printf("%f + %f = %f\n",
+                operand1,
+                operand2,
+                operand1 + operand2);
+            break;
 
-    case '-':
-        printf("%f - %f = %f\n",
-            operand1,
-            operand2,
-            operand1 - operand2);
-        break;
+        case '-':
+            printf("%f - %f = %f\n",
+                operand1,
+                operand2,
+                operand1 - operand2);
+            break;
 
-    case '*':
-        printf("%f * %f = %f\n",
-            operand1,
-            operand2,
-            operand1 * operand2);
-        break;
+        case '*':
+            printf("%f * %f = %f\n",
+                operand1,
+                operand2,
+                operand1 * operand2);
+            break;
 
-    case '/':
-        printf("%f / %f = %f\n",
-            operand1,
-            operand2,
-            operand1 / operand2);
-        break;
-    
-    default:
-        printf("Invalid operator entered.\n");
-    }
+        case '/':
+            if (operand2 == 0) {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1;
+            } else { 
+            printf("%f / %f = %f\n",
+                operand1,
+                operand2,
+                operand1 / operand2);
+            }
+            break;
 
-    return 0;
+        default:
+            printf("Invalid operator entered.\n");
+        } 
+        printf("Do you want to perform another calculation? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
 }
